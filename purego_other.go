03a6afe63ccbf86cf0402363c7efa4f513931916
Milan Nikolic <gen2brain@gmail.com>
2024-04-05 09:40:33 +0200

    Fix build for WASM

--- purego_other.go
+++ purego_other.go
@@ -4,9 +4,24 @@ package webp
 
 import (
 	"fmt"
+	"image"
+	"io"
 	"runtime"
 )
 
+var (
+	dynamic    = false
+	dynamicErr = fmt.Errorf("webp: unsupported os: %s", runtime.GOOS)
+)
+
+func decodeDynamic(r io.Reader, configOnly, decodeAll bool) (*WEBP, image.Config, error) {
+	return nil, image.Config{}, dynamicErr
+}
+
+func encodeDynamic(w io.Writer, m image.Image, quality, method int, lossless, exact bool) error {
+	return dynamicErr
+}
+
 func loadLibrary(name string) (uintptr, error) {
-	return 0, fmt.Errorf("unsupported os: %s", runtime.GOOS)
+	return 0, dynamicErr
 }
--- webp_dynamic.go
+++ webp_dynamic.go
@@ -1,3 +1,5 @@
+//go:build unix || darwin || windows
+
 package webp
 
 import (
